2482419a373c28b7d2c94c76e082d2ea86325e1a
reinelgiro123 <rci.bsis.competentegiro@gmail.com>
2026-02-10 23:23:38 +0800

    Connected to Neon Postgres and added env vars

--- valentinesite/wsgi.py
+++ valentinesite/wsgi.py
@@ -1,7 +1,17 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
+
+# Get the base directory
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'valentinesite.settings')
 
 application = get_wsgi_application()
-app = application  # Add this line! Vercel needs 'app'
+
+# ADD THIS LINE: It forces WhiteNoise to find your photos directly
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
+
+# Vercel needs this 'app' variable
+app = application
